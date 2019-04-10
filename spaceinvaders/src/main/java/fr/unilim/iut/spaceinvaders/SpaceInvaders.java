a5326d57798c5f829bef089642d1283f1f226268
VM IUT <vm@iut>
2019-04-10 17:21:53 +0200

    creation espace de jeux

--- spaceinvaders/src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ spaceinvaders/src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -8,4 +8,17 @@ public class SpaceInvaders {
 	   this.longueur = longueur;
 	   this.hauteur = hauteur;
    }
+    
+    
+    @Override
+	public String toString() {
+		StringBuilder espaceDeJeu = new StringBuilder();
+		for (int i = 0; i < hauteur; i++) {
+			for (int j = 0; j < longueur; j++) {
+				espaceDeJeu.append('.');
+			}
+			espaceDeJeu.append('\n');
+		}
+		return espaceDeJeu.toString();
+	}
 }
